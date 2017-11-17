1028ca661051e61bfd0aecdd9788e67f09edec95
KuanTingLin <woodoow226@gmail.com>
2017-11-17 15:43:17 +0800

    try two multithread , will it closed?

--- Momo_MultiThread.py
+++ Momo_MultiThread.py
@@ -10,7 +10,8 @@ import re
 _step_ = True
 URLsource = "https://www.momoshop.com.tw"
 URL = "https://www.momoshop.com.tw/category/LgrpCategory.jsp?l_code=1400300000"
-pageURL = "&lowestPrice=&highestPrice=&whcode=&cpyn=&p_pageNum=%s"
+pageURL1 = "&lowestPrice=&highestPrice=&whcode=&cpyn=&p_pageNum=%s"
+pageURL2 = "&p_orderType=4&lowestPrice=&highestPrice=&whcode=&cpyn=&p_pageNum=%s"
 listmomo = [] # 存商品頁網址
 listmomoall = [] # 存全部商品頁網址
 listmomo_goods = [] # 存單品網址
@@ -18,11 +19,11 @@ items_data = [] #存單品資料
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
 }
-
+queue = Queue()
 def worker():
     while not queue.empty():
-        page = queue.get()
-        crawler(page)
+        items = queue.get()
+        crawler(items)
 
 
 
@@ -64,47 +65,58 @@ if __name__ == "__main__":
         r_category = re.findall('/category/DgrpCategory.jsp\?d_code=.*bt_2_107.*ctype=B',s.select("a")[ss].get("href","no"))
         # 有些則是點進去就是單品
         r_goods = re.findall('/goods/GoodsDetail.jsp\?i_code=.*bt_2_107.*ctype=B',s.select("a")[ss].get("href","no"))
-        if len(r) > 0 :
+        if len(r_category) > 0 :
             listmomo.append(URLsource+r_category[0])
-        if len(r1) > 0 :
+        if len(r_goods) > 0 :
             listmomo_goods.append(URLsource+r_goods[0])
-
+    print(listmomo)
+    print(listmomo_goods)
     for i in range(len(listmomo)): # 將商品頁的每一頁網址抓下來
         res = requests.get(listmomo[i], headers=headers)
         s = bs(res.text, 'html5lib')
-        pages = re.findall("/(\d*)", s.select_one("div.pageArea > dl > dt > span").text)  # 商品頁數
-        int(pages[0]) # 轉成數字
-        r = re.findall('https://www.momoshop.com.tw/category/DgrpCategory.jsp\?d_code=\d*&p_orderType=1',listmomo[i])
-        for page in range(1,pages+1) :
-            r = r + pageURL%page #商品頁每一頁的網址
+        pages = re.findall("/(\d*)", s.select_one("div.pageArea > dl > dt > span").text)[0]  # 商品頁數
+        pages = int(pages) # 轉成數字
+        rls = re.findall('https://www.momoshop.com.tw/category/DgrpCategory.jsp\?d_code=\d*',listmomo[i])
+        if len(rls) < 1 :
+            rls = re.findall('https://www.momoshop.com.tw/category/DgrpCategory.jsp\?d_code=\d*',listmomo[i])[0]
+            r = rls[0] + pageURL2
+        else :
+            r = rls[0] + pageURL1
+        for page in range(1,(pages+1)):
+            r%page #商品頁每一頁的網址
             listmomoall.append(r)
+    pages = len(listmomoall)
+    numThread = 4
 
-
-
-    for i in range(items, items - 10, -1):  # 爬item動作
+    for i in range(pages, pages - 4, -1):  # 爬item動作
         queue.put(i)
     threads = []
     for j in range(numThread):  # 建立多執緒清單
-        threads.append(Thread(target=worker()))
+        threads.append(Thread(target=worker))
     for i in range(len(threads)):  # 將所有執行緒啟動，worker()開始到queue拿取工作
         threads[i].start()
+        print("run")
     for i in range(len(threads)):  # 等所有worker()工作完畢
         threads[i].join()
 
+    _step_ = False #切換
+    print("下半段")
+
     items = len(listmomo_goods)
-    for i in range(items, items-10, -1):  # 爬item動作
+    for i in range(items, items-1, -1):  # 爬item動作
         queue.put(i)
     threads = []
     for j in range(numThread):  # 建立多執緒清單
-        threads.append(Thread(target=worker()))
+        threads.append(Thread(target=worker))
     for i in range(len(threads)):  # 將所有執行緒啟動，worker()開始到queue拿取工作
         threads[i].start()
+        print("run2")
     for i in range(len(threads)):  # 等所有worker()工作完畢
         threads[i].join()
     thEnd = datetime.now()
     timeSpent = str(thEnd - thStart).split('.')[0]
 
-    with open('D:\Python\momo_Data_bikini.json', 'w', encoding="utf-8") as f:  # 將resList存為json檔
+    with open('D:\專題\momo_Data_bikini.json', 'w', encoding="utf-8") as f:  # 將resList存為json檔
         f.write(json.dumps(items_data, ensure_ascii=False, indent=4))
 
     print("執行緒:" + str(numThread))

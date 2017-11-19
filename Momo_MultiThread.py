51437e34aa2bf2b4aa35f1fc17a72ce8d60ce0d2
KuanTingLin <woodoow226@gmail.com>
2017-11-19 13:22:42 +0800

    now is good then i will try about running things

--- Momo_MultiThread.py
+++ Momo_MultiThread.py
@@ -6,7 +6,9 @@ from threading import Thread
 from datetime import datetime
 import json
 import re
+import time
 #需要設定的唷
+#l,m,d,i
 _step_ = True
 URLsource = "https://www.momoshop.com.tw"
 URL = "https://www.momoshop.com.tw/category/LgrpCategory.jsp?l_code=1400300000"
@@ -25,37 +27,37 @@ def worker():
         items = queue.get()
         crawler(items)
 
-
-
 def crawler(items):
     if _step_ == True:# 將商品頁的每一頁網址抓下來
-        for i in range(len(listmomo)):
-            res = requests.get(listmomo[i], headers=headers)
-            s = bs(res.text, 'html5lib')
-            eachgood = s.select("div.prdListArea > ul > li.eachGood > a")  # 單頁商品數
-            for j in range(len(eachgood)):
-                listmomo_goods.append(URLsource + eachgood[j].get("href", "no"))
+        print("now is " + str(items))
+        res = requests.get(listmomoall[items], headers=headers)
+        s = bs(res.text, 'html5lib')
+        eachgood = s.select("div.prdListArea > ul > li.eachGood > a")  # 單頁商品數
+        for j in range(len(eachgood)):
+            listmomo_goods.append(URLsource + eachgood[j].get("href", "no"))
+            print("now is go  " + str(items) + ":" + str(j))
+            time.sleep(0.001)
     else :# 抓所有品項資訊
-        for item in range(items):
-            res = requests.get(listmomo_goods[item], headers=headers)
-            s = bs(res.text, 'html5lib')
-            title = s.select_one("h1").text
-            Catalog = []
-            sCatalog = s.select("#bt_2_layout_NAV > ul > li")
-            for i in range(len(sCatalog)):
-                Catalog.append(sCatalog[i].text)
-            del_price = s.select("ul.prdPrice > li")[0].select("del")
-            if len(del_price) > 0:
-                del_price = s.select("ul.prdPrice > li")[0].select("del")[0].text
-                now_price = s.select("ul.prdPrice > li")[1].select("span")[0].text
-            else:
-                del_price = ""
-                now_price = s.select("ul.prdPrice > li")[0].select("span")[0].text
-
-            item_data = {"title": title, "catalog": Catalog,
-                         "price": [{"del_price": del_price, 'now_price': now_price}]}
-            items_data.append(item_data)
-    print("item " + str(page) + " crawler done")
+        print("now are " + str(items))
+        res = requests.get(listmomo_goods[items], headers=headers)
+        s = bs(res.text, 'html5lib')
+        title = s.select_one("h1").text
+        Catalog = []
+        sCatalog = s.select("#bt_2_layout_NAV > ul > li")
+        for i in range(len(sCatalog)):
+            Catalog.append(sCatalog[i].text)
+        del_price = s.select("ul.prdPrice > li")[0].select("del")
+        if len(del_price) > 0:
+            del_price = s.select("ul.prdPrice > li")[0].select("del")[0].text
+            now_price = s.select("ul.prdPrice > li")[1].select("span")[0].text
+        else:
+            del_price = ""
+            now_price = s.select("ul.prdPrice > li")[0].select("span")[0].text
+        print("now are go  " + str(items) + ":")
+        item_data = {"title": title, "catalog": Catalog,
+                     "price": [{"del_price": del_price, 'now_price': now_price}]}
+        items_data.append(item_data)
+    print("item " + str(items) + " crawler done")
 
 if __name__ == "__main__":
     thStart = datetime.now()
@@ -86,10 +88,13 @@ if __name__ == "__main__":
         for page in range(1,(pages+1)):
             r%page #商品頁每一頁的網址
             listmomoall.append(r)
+    thEnd = datetime.now()
+    timeSpent = str(thEnd - thStart).split('.')[0]
+    print("first = " + timeSpent);
     pages = len(listmomoall)
     numThread = 4
 
-    for i in range(1,4):  # 爬item動作
+    for i in range(1,5):  # 爬item動作
         queue.put(i)
     threads = []
     for j in range(numThread):  # 建立多執緒清單
@@ -100,11 +105,14 @@ if __name__ == "__main__":
     for i in range(len(threads)):  # 等所有worker()工作完畢
         threads[i].join()
 
+    thEnd = datetime.now()
+    timeSpent = str(thEnd - thStart).split('.')[0]
+    print("upper = " + timeSpent);
     _step_ = False #切換
     print("下半段")
 
     items = len(listmomo_goods)
-    for i in range(1,4):  # 爬item動作
+    for i in range(1,5):  # 爬item動作
         queue.put(i)
     threads = []
     for j in range(numThread):  # 建立多執緒清單
@@ -117,7 +125,7 @@ if __name__ == "__main__":
     thEnd = datetime.now()
     timeSpent = str(thEnd - thStart).split('.')[0]
 
-    with open('D:\專題\momo_Data_bikini.json', 'w', encoding="utf-8") as f:  # 將resList存為json檔
+    with open('D:\專題\momo_Data_roadrun.json', 'w', encoding="utf-8") as f:  # 將resList存為json檔
         f.write(json.dumps(items_data, ensure_ascii=False, indent=4))
 
     print("執行緒:" + str(numThread))

afbf162c9978d9f8d6eadc8477857b514c15622e
KuanTingLin <woodoow226@gmail.com>
2017-12-09 20:12:39 +0800

    raspberry heart beat sensor test

--- heart.py
+++ heart.py
@@ -14,33 +14,16 @@ def ReadChannel(channel):
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
-def BPM(num_of_beats):
-    rate = num_of_beats / 5
-    bpm = rate * 60
-    return bpm
-
 data_channel = 0
 threshold = 100
 def main():
     print("heart is beating")
+    # input data as a csv file
     csvfile = csv.writer(open('./heart.csv', 'w'))
     csvfile.writerow(['Timestamp_RPI', 'heart_beat'])
     try:
         while True:
             csvfile.writerow([datetime.now().isoformat(), ReadChannel(data_channel)])
-            # beat = 0
-            # start_time = time.time()
-            # print(ReadChannel(data_channel))
-            # prev_data = ReadChannel(data_channel)
-            # while time.time() - start_time < 5:
-            #     current_data = ReadChannel(data_channel)
-            #
-            #     if current_data - prev_data > threshold:
-            #         beat = beat + 1
-            #     prev_data = current_data
-            #
-            # bpm = BPM(beat)
-            # print("Beats Per Minute:{}".format(bpm))
             time.sleep(0.1)
 
     except KeyboardInterrupt:
@@ -48,6 +31,7 @@ def main():
 
     finally:
         GPIO.cleanup()
+        # clear
 
 # call main
 if __name__ == '__main__':

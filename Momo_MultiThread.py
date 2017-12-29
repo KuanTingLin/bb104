a779d0ace4724155f3a0427429c5ce6846254a0a
KuanTingLin <woodoow226@gmail.com>
2017-12-29 20:26:50 +0800

    crawler with multi threads

--- Momo_MultiThread.py
+++ Momo_MultiThread.py
@@ -1,102 +1,113 @@
 # 起手式
 import requests
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 # from queue import Queue
 # from threading import Thread
 from concurrent.futures import ThreadPoolExecutor, wait
 from datetime import datetime
 import json
 import re
-import time
-#需要設定的唷
-#l,m,d,i
+
 _step_ = True
-URLsource = "https://www.momoshop.com.tw"
-URL = "https://www.momoshop.com.tw/category/LgrpCategory.jsp?l_code=1400300000"
-pageURL1 = "&lowestPrice=&highestPrice=&whcode=&cpyn=&p_pageNum=%s"
-pageURL2 = "&p_orderType=4&lowestPrice=&highestPrice=&whcode=&cpyn=&p_pageNum=%s"
-listmomo = [] # 存商品頁網址
-listmomoall = [] # 存全部商品頁網址
-listmomo_goods = [] # 存單品網址
-items_data = [] #存單品資料
-headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-}
+
+
 # queue = Queue()
 # def worker():
 #     while not queue.empty():
 #         items = queue.get()
 #         crawler(items)
-def crawler2(items):
-    res = requests.get(listmomo_goods[items], headers=headers)
-    s = bs(res.text, 'html5lib')
-    s_price = s.select("ul.prdPrice > li")
-    title = s.select_one("h1").text
-    Catalog = []
-    sCatalog = s.select("#bt_2_layout_NAV > ul > li")
-    for i in range(len(sCatalog)):
-        Catalog.append(sCatalog[i].text)
-    del_price = s_price[0].select("del")
-    if len(del_price) > 0:
-        del_price = del_price[0].text
-        now_price = s_price[1].select("span")[0].text
-    else:
-        del_price = ""
-        now_price = s_price[0].select("span")[0].text
-    print("now are go  " + str(items) + ":")
-    item_data = {"title": title, "catalog": Catalog,
-                 "price": [{"del_price": del_price, 'now_price': now_price}]}
-    items_data.append(item_data)
-    print("item " + str(items) + " crawler done")
-def crawler(items):
-    res = requests.get(listmomoall[items], headers=headers)
-    s = bs(res.text, 'html5lib')
-    eachgood = s.select("div.prdListArea > ul > li.eachGood > a")  # 單頁商品數
-    for j in range(len(eachgood)):
-        listmomo_goods.append(URLsource + eachgood[j].get("href", "no"))
-    print("itemspage :  " + str(items) + " crawler done")
+class Crawler():
+    # def __init__(self): #當想要在呼叫時就做某些事的時候才使用
+    #     pass
+    def craw_item_data(self, items, datas, headers):
+        resp = requests.get(items, headers=headers)
+        soup = BeautifulSoup(resp.text, 'html5lib')
+
+        li_prices = soup.select("ul.prdPrice > li")
+        title = soup.select_one("h1").text
+        li_categories = soup.select("#bt_2_layout_NAV > ul > li")
+        categories = []
+        for li_category in li_categories:
+            categories.append(li_category.text)
+
+        del_price = li_prices[0].select("del")
+        if len(del_price):
+            price = del_price[0].text
+            discount_price = li_prices[1].select("span")[0].text
+        else:
+            price = ""
+            discount_price = li_prices[0].select("span")[0].text
+        print("now are go  " + str(items) + ":")
+        item_data = {"title": title, "categories": categories,
+                     "price": [{"price": price, 'discount price': discount_price}]}
+        datas.append(item_data)
+        print("Done crawling item: " + str(items))
+
+    def craw_goods_url(self, items, goods, headers, url_source):
+        res = requests.get(items, headers=headers)
+        soup = BeautifulSoup(res.text, 'html5lib')
+        eachgood = soup.select("div.prdListArea > ul > li.eachGood > a")  # 單頁商品數
+        for good in eachgood:
+            goods.append(url_source + good.get("href", "no"))
+        print("itemspage :  " + str(items) + " crawler done")
+
 
 def main():
-    thStart = datetime.now()
-    resp = requests.get(URL,headers = headers)
-    s = bs(resp.text, 'html5lib')
-    for ss in range(len(s.select("a"))): # 抓主頁
+    DOMAIN = "https://www.momoshop.com.tw"
+    URL = "https://www.momoshop.com.tw/category/LgrpCategory.jsp?l_code=1400300000"
+    URL_PATTERN_1 = "&lowestPrice=&highestPrice=&whcode=&cpyn=&p_pageNum=%s"
+    URL_PATTERN_2 = "&p_orderType=4&lowestPrice=&highestPrice=&whcode=&cpyn=&p_pageNum=%s"
+    category_urls = []  # 存各類別商品第一頁網址
+    product_urls = []  # 存全部商品頁網址
+    item_urls = []  # 存單品網址
+    item_datas = []  # 存單品資料
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    }
+    th_start = datetime.now()
+    resp = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(resp.text, 'html5lib')
+    for a in soup.select("a"):  # 抓主頁
         # 有些內容點進去是該商家商品頁  bt_2_107
-        r_category = re.findall('/category/DgrpCategory.jsp\?d_code=.*bt_2_107.*ctype=B',s.select("a")[ss].get("href","no"))
+        r_category = re.findall('/category/DgrpCategory.jsp\?d_code=.*bt_2_107.*ctype=B',
+                                a.get("href", "no"))
         # 有些則是點進去就是單品
-        r_goods = re.findall('/goods/GoodsDetail.jsp\?i_code=.*bt_2_107.*ctype=B',s.select("a")[ss].get("href","no"))
-        if len(r_category) > 0 :
-            listmomo.append(URLsource+r_category[0])
-        if len(r_goods) > 0 :
-            listmomo_goods.append(URLsource+r_goods[0])
-    print(listmomo)
-    print(listmomo_goods)
-    for i in range(len(listmomo)): # 將每個商品頁的每一頁網址抓下來
-        res = requests.get(listmomo[i], headers=headers)
-        s = bs(res.text, 'html5lib')
-        pages = re.findall("/(\d*)", s.select_one("div.pageArea > dl > dt > span").text)[0]  # 商品頁數
-        pages = int(pages) # 轉成數字
-        rls = re.findall('https://www.momoshop.com.tw/category/DgrpCategory.jsp\?d_code=\d*',listmomo[i])
-        if len(rls) < 1 :
-            rls = re.findall('https://www.momoshop.com.tw/category/DgrpCategory.jsp\?d_code=\d*',listmomo[i])[0]
-            r = rls[0] + pageURL2
-        else :
-            r = rls[0] + pageURL1
-        for page in range(1,(pages+1)):
+        r_goods = re.findall('/goods/GoodsDetail.jsp\?i_code=.*bt_2_107.*ctype=B', a.get("href", "no"))
+        if len(r_category) > 0:
+            category_urls.append(DOMAIN + r_category[0])
+        if len(r_goods) > 0:
+            item_urls.append(DOMAIN + r_goods[0])
+    print(category_urls)
+    print(item_urls)
+    for i in range(len(category_urls)):  # 將每個商品頁的每一頁網址抓下來
+        resp = requests.get(category_urls[i], headers=headers)
+        soup = BeautifulSoup(resp.text, 'html5lib')
+        pages = re.findall("/(\d*)", soup.select_one("div.pageArea > dl > dt > span").text)[0]  # 商品頁數
+        pages = int(pages)  # 轉成數字
+        r_urls = re.findall('https://www.momoshop.com.tw/category/DgrpCategory.jsp\?d_code=\d*', category_urls[i])
+        if len(r_urls) < 1:
+            r_urls = re.findall('https://www.momoshop.com.tw/category/DgrpCategory.jsp\?d_code=\d*', category_urls[i])[0]
+            r = r_urls[0] + URL_PATTERN_2
+        else:
+            r = r_urls[0] + URL_PATTERN_1
+        for page in range(1, (pages + 1)):
             # r%page #商品頁每一頁的網址
-            listmomoall.append(r%page)
+            product_urls.append(r % page)
 
     ###計時
-    thEnd = datetime.now()
-    timeSpent = str(thEnd - thStart).split('.')[0]
-    print("first = " + timeSpent)
+    th_end = datetime.now()
+    time_spent = str(th_end - th_start).split('.')[0]
+    print("first = " + time_spent)
     ###
-    pages = len(listmomoall)
-    Thread_num = 4 #多執行緒數目
+    pages = len(product_urls)
+    Thread_num = 4  # 多執行緒數目
     print(pages)
+
     threads = ThreadPoolExecutor(Thread_num)  # 設定多執行緒
-    futures = [threads.submit(crawler, page) for page in range(pages)]  # 將工作事項交給futures管理
-    wait(futures)
+    crawler = Crawler()
+    futures = [threads.submit(crawler.craw_goods_url, product_urls[page], item_urls, headers, DOMAIN)
+               for page in range(pages)]  # 將工作事項交給futures管理
+    wait(futures) # 等待工作完成，此程序才會繼續動作
     # for i in range(1,pages):  # 爬page動作
     #     queue.put(i)
     # threads = []
@@ -107,16 +118,17 @@ def main():
     #     print("run")
     # for i in range(len(threads)):  # 等所有worker()工作完畢
     #     threads[i].join()
-    ### 計時
-    thEnd = datetime.now()
-    timeSpent = str(thEnd - thStart).split('.')[0]
-    print("upper = " + timeSpent)
+    th_end = datetime.now()  ### 計時
+    time_spent = str(th_end - th_start).split('.')[0]
+    print("upper = " + time_spent)
     ###
-    items = len(listmomo_goods)
+    items = len(item_urls)
     print(items)
     threads = ThreadPoolExecutor(Thread_num)  # 設定多執行緒
-    futures = [threads.submit(crawler2, item) for item in range(items)]  # 將工作事項交給futures管理
-    wait(futures) #等待工作完成，此程序才會繼續動作
+    futures = [threads.submit(crawler.craw_item_data, item_urls[item], item_datas, headers) for item in
+               range(items)]  # 將工作事項交給futures管理
+    wait(futures)  # 等待工作完成，此程序才會繼續動作
+
     # for i in range(1,items+1):  # 爬item動作
     #     queue.put(i)
     # threads = []
@@ -127,16 +139,17 @@ def main():
     #     print("run2")
     # for i in range(len(threads)):  # 等所有worker()工作完畢
     #     threads[i].join()
-    # 時間結束
-    thEnd = datetime.now()
-    timeSpent = str(thEnd - thStart).split('.')[0]
+
+    th_end = datetime.now()  # 時間結束
+    time_spent = str(th_end - th_start).split('.')[0]
 
     with open('D:\專題\momo_Data_roadrun.json', 'w', encoding="utf-8") as f:  # 將resList存為json檔
-        f.write(json.dumps(items_data, ensure_ascii=False, indent=4))
+        f.write(json.dumps(item_datas, ensure_ascii=False, indent=4))
 
     print("執行緒:" + str(Thread_num))
-    print("筆數:" + str(len(items_data)))
-    print("耗時:" + timeSpent)
+    print("筆數:" + str(len(item_datas)))
+    print("耗時:" + time_spent)
+
 
 if __name__ == "__main__":
     main()
